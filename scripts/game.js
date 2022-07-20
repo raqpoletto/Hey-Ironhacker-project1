956a9821c35dc9573dda7546a419e666f2e92340
raqpoletto <polettoraq@gmail.com>
2022-07-20 09:07:41 +0100

    no progress

--- scripts/game.js
+++ scripts/game.js
@@ -3,30 +3,53 @@
 class Game {
   constructor(words, ctx) {
     this.ctx = ctx;
-    this.words = words;
-    this.currentAnswer = words[0].answer;
-    this.currentHint = words[0].hint;
-    this.erros = 0;
-    this.acertos = 0;
-    this.tentativas = [];
-    this.interval = null;
-    this.img = null;
-    const img = new Image();
+    this.words = words; //array of objects on main
+    this.currentAnswer = words[0].answer; //current secret word(prop)
+    this.currentHint = words[0].hint; //current hint(prop)
+    this.erros = 0; //player mistakes
+    this.acertos = 0; //player correct letters
+    this.tentativas = []; //array of letters choosen by player
+    this.interval = null; //interval to start game
+    this.img = null; //img related to hint
+    const img = new Image(); //to load the img on canvas
     img.addEventListener("load", () => {});
     img.src = this.currentHint;
     this.img = img;
     this.isRunning = false;
   }
 
+
   start = () => {
     this.interval = setInterval(this.updateGame, 1000 / 60);
     this.isRunning = true;
   };
 
+  /*  restartGame = () => {
+    this.ctx.clearRect(0, 0, 1000, 500);
+    if (erros >= 5) {
+      this.ctx.font = "32px sans serif";
+      this.ctx.fillStyle = "black";
+      this.ctx.fillText("Try again", 200, 100);
+      return;
+    }
+    if (this.acertos === this.words.length) {
+      this.ctx.font = "32px sans serif";
+      this.ctx.fillStyle = "black";
+      this.ctx.fillText("You won", 200, 100);
+      return;
+    }
+  }; created this if statement to restart the game automatically every time the playes gets 5 mistakes */
+
+  displaySentence() {
+    this.ctx.fillStyle = "black";
+    let newSentence = words.sentence[0];
+    this.ctx.fillText(newSentence, 100, 100);
+  } // is not working // why?
+
   displayText() {
     this.ctx.fillStyle = "black";
     let hiddenWord = "_ ".repeat(this.currentAnswer.length);
-    this.ctx.fillText(hiddenWord, 100, 100);
+    this.ctx.fillText(hiddenWord, 500, 100);
   }
 
   displayHint() {
@@ -42,34 +65,45 @@ class Game {
     this.tentativas.push(key);
     console.log(this.tentativas);
   }
-
+  
   displayTentativas() {
     let allTentativas = this.tentativas.join("-");
     this.ctx.fillText("You tried: " + allTentativas, 400, 400);
   }
 
+  let checkIfPlayerWins = this.alltentativas.map(this.tentativas => {
+    checkPlayerAnswer() {
+      if (this.acertos === words.answer.length) {
+        return this.ctx.fillText("you win, desgraça", 100, 100);
+      } else if (this.erros >= 5) {
+        return this.ctx.fillText("try again, querida", 100, 100);
+      }
+    }
+  })
+
+  
+  
+  /* 
+  console.log(this.tentativas.join( ));
+
+    this.ctx.fillText(this.tentativas, 400, 350);
+  } I need to transforme the array of tentativas to string so I able to print on canvas induviduals letters (above the dashes)*/
+
   updateGame = () => {
-    this.ctx.clearRect(0, 0, 1000, 500);
+    this.ctx.clearRect(0, 0, 900, 500);
     this.displayText();
     this.displayHint();
     this.displayTentativas();
   };
 }
-resetGame = () => {
-  this.ctx.clearRect(0, 0, 1000, 500);
-  if (erros >= 5) {
-    this.ctx.font = "32px sans serif";
-    this.ctx.fillStyle = "black";
-    this.ctx.fillText("Try again", 200, 100);
-    return;
+
+/* function playerAnswer(key) {
+  if ((key === words.answer[i])) {
+    return true;
+  } else {
+    return false;
   }
-  if (this.acertos === this.words.length) {
-    this.ctx.font = "32px sans serif";
-    this.ctx.fillStyle = "black";
-    this.ctx.fillText("You won", 200, 100);
-    return;
-  }
-};
+} */
 
 /* const canvas = document.getElementById("main"); */
 /* canvas.width = window.innerWidth;
--- scripts/main.js
+++ scripts/main.js
@@ -11,18 +11,36 @@ ctx.fillText("Click enter", 400, 50);
 let words = [
   {
     answer: "mind",
-    hint: "https://i.kym-cdn.com/entries/icons/facebook/000/002/232/bullet_cat.jpg",
+    hint: "https://www.puzzlegamemaster.com/wp-content/uploads/2020/06/MIND-160x160.jpg",
+    sentence: "Hey, ironhacker! What's on your ",
   },
-  "alone",
-  "ironhack",
-  "web",
-  "lisbon",
+  {
+    answer: "alone",
+    hint: "https://www.puzzlegamemaster.com/wp-content/uploads/2020/06/ALONE-160x160.jpg",
+    sentence: "Don't code all ",
+  },
+  {
+    answer: "parties",
+    hint: "https://www.puzzlegamemaster.com/wp-content/uploads/2020/06/PARTIES-160x160.jpg",
+    sentence: "We know you are missing the summer and the ",
+  },
+  {
+    answer: "lisbon",
+    hint: (ctx.fillStyle = black),
+    sentence: " is waiting for you, little dev.",
+  },
+  {
+    answer: "around",
+    hint: "https://www.puzzlegamemaster.com/wp-content/uploads/2020/06/AROUND-160x160.jpg",
+    sentence: "dont know yet",
+  },
 ];
 
 const startGameBtn = document.getElementById("button-start");
 
 let game = new Game(words, ctx);
 
+// to start
 document.addEventListener("keydown", (e) => {
   if (e.code === "Enter" && !game.isRunning) {
     game.start();
@@ -31,6 +49,11 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-/* startGameBtn.addEventListener('click', () => {
-  
-}) */
+// to restart
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space" && game.isRunning) {
+    game.updateGame();
+  } else {
+    game.checkKey(e.key);
+  }
+});
